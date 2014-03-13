f725ceeaa2c6355af5f71105fdc2e91525384ad4
Philipp Volugine <phil.volguine@gmail.com>
2014-03-13 15:18:25 -0400

    renamed read_settings to settings, settings filepath fix
    settings now tries to read __main__.__file__ rather
    than just __file__

--- common_components/settings.py
+++ common_components/settings.py
@@ -3,7 +3,10 @@ import os
 def read_settings(filepath=None):
     from json import loads
     if filepath is None:
-        here = os.path.dirname(__file__)
+        if hasattr(__main__, '__file__'):
+            here = os.path.dirname(__main__.__file__)
+        else:
+            here = os.path.dirname(__file__)
         filepath = os.path.join(here, 'settings.local.json')
         if not os.path.exists(filepath):
             filepath = os.path.join(here, 'settings.json')
@@ -18,4 +21,3 @@ class Settings(dict):
         dict.__init__(self)
         self.update(read_settings())
         self['project_dir'] = os.path.dirname(__file__)
-
